8e594fa78338d23d3f6300b37155d232d20da284
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-21 22:31:07 +0500

    feat(routes): add stars page to display favorite lessons

---
+++ app/(routes)/stars/page.tsx
@@ -0,0 +1,8 @@
+export default function Stars() {
+  return (
+    <header className="mb-6 lg:mb-8">
+      <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-800 mb-3">Stars</h2>
+    </header>
+  )
+}
+
